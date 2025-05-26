6f581af9457f7340aa5403fc34f956aa0fc120b5
Lucas <xablau2007pvp@gmail.com>
2025-05-26 20:22:06 -0300

    hire me page modified

--- src/app/ui/Header.jsx
+++ src/app/ui/Header.jsx
@@ -1,34 +1,34 @@
-import Link from "next/link"
-import { Button } from "@/components/ui/button"
-import Nav from "./Nav"
-import MobileNav from "./MobileNav"
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
-    return (
-        <header className="py-8 xl:py-12 text-white">
-            <div className="container mx-auto flex justify-between items-center">
-                { /* Logo */}
-                <Link href="/">
-                    <h1 className="text-4xl">
-                        LucasAnselmo<span className="text-blue-500">044</span>
-                    </h1>
-                </Link>
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <Link href="/">
+          <h1 className="text-4xl">
+            LucasAnselmo<span className="text-blue-500">044</span>
+          </h1>
+        </Link>
 
-                { /* desktop nav */}
-                <div className="hidden xl:flex items-center gap-8">
-                    <Nav />
-                    <Link href="/contact">
-                        <Button>Hire me</Button>
-                    </Link>
-                </div>
+        {/* desktop nav */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="https://www.linkedin.com/in/lucas-anselmo-3568752a2/">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
 
-                { /* mobile nav */}
-                <div className="xl:hidden ">
-                    <MobileNav />
-                </div>
-            </div>
-        </header>
-    )
-}
+        {/* mobile nav */}
+        <div className="xl:hidden ">
+          <MobileNav />
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/app/ui/Photo.jsx
+++ src/app/ui/Photo.jsx
@@ -27,7 +27,7 @@ const Photo = () => {
             quality={100}
             fill
             alt=""
-            className="object-contain"
+            className="object-cover rounded-full"
           />
         </motion.div>
 
